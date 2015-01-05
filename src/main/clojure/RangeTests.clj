2181cde2ba0f1a692f1fafd94e0bebe738c472f8
izeye <izeye@naver.com>
2015-01-05 15:06:01 +0900

    Add a range sample from https://clojuredocs.org/clojure.core/range.

---
+++ src/main/clojure/RangeTests.clj
@@ -0,0 +1,26 @@
+
+;(println (range))
+
+(println (take 10 (range)))
+
+(println (range 10))
+
+(println (range -5 5))
+
+(println (range -100 100 10))
+
+(println (range 0 4 2))
+
+(println (range 0 5 2))
+
+(println (range 0 6 2))
+
+(println (range 0 7 2))
+
+(println (range 100 0 -10))
+
+(println (range 10 -10 -1))
+
+(println (take 5 (range 42 (java.lang.Integer/MAX_VALUE))))
+
+(println (take 5 (drop 42 (range))))
